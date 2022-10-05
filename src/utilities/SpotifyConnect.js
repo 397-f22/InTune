51826acbe2b6914e05d286def254166f2b938171
Adam Stewart <adamjoesph11@gmail.com>
2022-10-05 12:45:15 -0500

    now returns api resp frop spotify through BE

--- src/utilities/SpotifyConnect.js
+++ src/utilities/SpotifyConnect.js
@@ -16,26 +16,25 @@ import { json } from "react-router-dom";
 //     return(<h1>hello</h1>);
 // }
 export const searchPlaylist = async (weather) => {
-    var data ;
-   await fetch('/api').then(res => {
-        if(res.ok){
+    var data;
+    await fetch('/api').then(res => {
+        if (res.ok) {
             return res.json()
         }
     }).then(jsonResponse => {
         console.log(jsonResponse.token)
         data = axios.get("https://api.spotify.com/v1/search", {
-        headers: {
-            Authorization: `Bearer ${jsonResponse.token}`
-        },
-        params: {
-            q: weather,
-            type: "playlist",
-            limit: 1
-        }
+            headers: {
+                Authorization: `Bearer ${jsonResponse.token}`
+            },
+            params: {
+                q: weather,
+                type: "playlist",
+                limit: 1
+            }
+        }).then(function (reponse) {
+            return reponse.data
+        })
     })
-    })
-
-    
-    console.log(data)
-    return data.playlists.items[0]
+    return data.then(resp => {return resp.playlists.items[0]})
 }
