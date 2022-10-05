bbd4da8edea888ea8d1a518b6848298609f4b339
Christopher Song <chris4728.song2001@gmail.com>
2022-10-04 19:32:18 -0500

    Feat: Add support for weather state variable

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,16 @@
 import "./LandingPage.css";
 
-const weatherTypes = ["sunny", "cloudy", "rainy", "snowy", "windy"];
+const weatherTypes = ["", "sunny", "cloudy", "rainy", "snowy", "windy"];
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
-const LandingPage = () => (
+const LandingPage = ({ weather, setWeather }) => (
   <div className="weather-select-dropdown">
     <label className="weather-select-label">Select Weather:</label>
-    <select className="select-bar">
+    <select className="select-bar" defaultValue={weather} onChange={(e) => setWeather(e.target.value)}>
       {
-        weatherTypes.map((weather) => (
-          <option value={weather}>{capitalize(weather)}</option>
+        weatherTypes.map((weatherType, id) => (
+          <option key={id} value={weatherType}>{capitalize(weatherType)}</option>
         ))
       }
     </select>
