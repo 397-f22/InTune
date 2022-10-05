a14928924072f7523128c35fa6b058001ca35925
Christopher Song <chris4728.song2001@gmail.com>
2022-10-04 19:47:10 -0500

    Feat: Add submit button to view songs recs (next page todo...)

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,16 +4,26 @@ const weatherTypes = ["", "sunny", "cloudy", "rainy", "snowy", "windy"];
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
+const handleSubmit = (weather) => {
+  if (weather === "") {
+    alert("Please select a weather type.");
+    return;
+  };
+}
+
 const LandingPage = ({ weather, setWeather }) => (
-  <div className="weather-select-dropdown">
-    <label className="weather-select-label">Select Weather:</label>
-    <select className="select-bar" defaultValue={weather} onChange={(e) => setWeather(e.target.value)}>
-      {
-        weatherTypes.map((weatherType, id) => (
-          <option key={id} value={weatherType}>{capitalize(weatherType)}</option>
-        ))
-      }
-    </select>
+  <div className="landing-page">
+    <div className="weather-select-dropdown">
+      <label className="weather-select-label">Select Weather:</label>
+      <select className="select-bar" defaultValue={weather} onChange={(e) => setWeather(e.target.value)}>
+        {
+          weatherTypes.map((weatherType, id) => (
+            <option key={id} value={weatherType}>{capitalize(weatherType)}</option>
+          ))
+        }
+      </select>
+    </div>
+    <input onClick={() => handleSubmit(weather)} className="weather-submit-button" type="button" value="View Songs!"></input>
   </div>
 );
 
