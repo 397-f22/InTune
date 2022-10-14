6c3d47ca6e6b90b14a26dbe5c6840eb77e1b1dda
Adam Stewart <adamjoesph11@gmail.com>
2022-10-14 12:46:08 -0500

    adding CSS for playlist link and removing duplicate token calls

--- src/utilities/SpotifyConnect.js
+++ src/utilities/SpotifyConnect.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const SERVER = 'https://us-central1-intune-ab424.cloudfunctions.net/app';
 
-
+var token;
 export const timeOfDay = () => {
     const today = new Date();
     const time = today.getHours()
-    return  (6<=time && time<12) ? "Morning" :
-            (12<=time && time<16) ? "Afternoon":
-            (16<=time && time<22) ? "Evening":
-           "Night"
+    return (6 <= time && time < 12) ? "Morning" :
+        (12 <= time && time < 16) ? "Afternoon" :
+            (16 <= time && time < 22) ? "Evening" :
+                "Night"
 
 }
 
@@ -17,42 +17,73 @@ export const searchPlaylist = async (weather) => {
     var data;
     const queryRange = 5
     console.log(timeOfDay())
-    await fetch(SERVER + '/api').then(res => {
-        if (res.ok) {
-            return res.json()
-        }
-    }).then(jsonResponse => {
+    if (!token) {
+        await fetch(SERVER + '/api').then(res => {
+            if (res.ok) {
+                let jsonresp = res.json()
+                token = jsonresp.token
+                return jsonresp
+            }
+        }).then(jsonResponse => {
+            data = axios.get("https://api.spotify.com/v1/search", {
+                headers: {
+                    Authorization: `Bearer ${jsonResponse.token}`
+                },
+                params: {
+                    q: weather + " " + timeOfDay(),
+                    type: "playlist",
+                    limit: queryRange
+                }
+            }).then(function (reponse) {
+                return reponse.data
+            })
+        })
+    }
+    else {//Have token
         data = axios.get("https://api.spotify.com/v1/search", {
             headers: {
-                Authorization: `Bearer ${jsonResponse.token}`
+                Authorization: `Bearer ${token}`
             },
             params: {
-                q: weather +" "+ timeOfDay(),
+                q: weather + " " + timeOfDay(),
                 type: "playlist",
                 limit: queryRange
             }
         }).then(function (reponse) {
             return reponse.data
         })
-    })
-    return data.then(resp => {return resp.playlists.items[Math.floor(Math.random()*queryRange)]})
+    }
+    return data.then(resp => { return resp.playlists.items[Math.floor(Math.random() * queryRange)] })
 }
 
 export const findSongs = async (playlist) => {
     var data;
-    await fetch(SERVER + '/api').then(res => {
-        if (res.ok) {
-            return res.json()
-        }
-    }).then(jsonResponse => {
-        data = axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
-            headers: {
-                Authorization: `Bearer ${jsonResponse.token}`
+    if (!token) {
+        await fetch(SERVER + '/api').then(res => {
+            if (res.ok) {
+                let jsonresp = res.json()
+                token = jsonresp.token
+                return jsonresp
             }
-        }).then(function (response) {
-            return response.data
+        }).then(jsonResponse => {
+            data = axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
+                headers: {
+                    Authorization: `Bearer ${jsonResponse.token}`
+                }
+            }).then(function (response) {
+                return response.data
+            })
         })
-    })
+    }
+    else{
+        data = axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(function (response) {
+                return response.data
+            })
+    }
     let songs = data.then(resp => {
         let vals = [];
         let result_songs = [];
@@ -64,9 +95,9 @@ export const findSongs = async (playlist) => {
                 vals.push(random)
             }
         }
-        
+
         return result_songs
-        })
-    
+    })
+
     return songs
 }
